88b5af85a65d66d42f831bd6c5746fd054cd84e8
Nitin <Nitin@VAIO>
2013-12-04 03:35:10 +0530

    Adding a stub for Simple Json Web Service

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        include_once 'HelloWorld/index.php';
+        include_once 'SimpleJsonWebService/SimpleJsonWebService.php';
         ?>
     </body>
 </html>
