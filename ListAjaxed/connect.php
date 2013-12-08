563fa220d90fa3b678fbc57f2dcade89adf32fc4
Nitin <Nitin@VAIO>
2013-12-09 01:41:27 +0530

    Minor changes

--- ListAjaxed/connect.php
+++ ListAjaxed/connect.php
@@ -1,17 +1,12 @@
 <?php
-
 /* Database config */
-
 $db_host	= 'localhost';
 $db_user	= 'root';
 $db_pass	= '';
 $db_database	= 'webservicedb'; 
-
 /* End config */
 
-
 $link = @mysql_connect($db_host,$db_user,$db_pass) or die('Unable to establish a DB connection');
-
 mysql_set_charset('utf8');
 mysql_select_db($db_database,$link) or die('Unable to select DB');
 
