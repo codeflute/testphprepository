d07601bc7e8b54229099f3a6a3a255bf7c88b731
Nitin <Nitin@VAIO>
2013-12-23 03:01:18 +0530

    Misc changes

--- index.php
+++ index.php
@@ -10,7 +10,10 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        <a href="HelloWorld/index.php">Hello World</a>
         <a href="SimpleJsonWebService/SimpleJsonWebService.php">Simple Json Web Service</a>
         <a href="ListAjaxed/demo.php">List Ajaxed</a>
+        <a href="SimpleMVC">Simple MVC</a>
+        <a href="Basic Joke DB/basic/ch7/admin/index.html">Joke DB</a>
     </body>
 </html>
